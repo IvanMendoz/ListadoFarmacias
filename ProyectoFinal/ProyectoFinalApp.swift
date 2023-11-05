d8a6d1f8c56c2396edeff4a3214272c268c6a58e
Ivan <ivanmendoza572@gmail.com>
2023-11-04 20:38:43 -0600

    Initial Commit

---
+++ ProyectoFinal/ProyectoFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoFinalApp.swift
+//  ProyectoFinal
+//
+//  Created by Gaby on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
